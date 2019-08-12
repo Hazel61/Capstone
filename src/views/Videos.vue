5371544bd9afc931704d9fb278ac75aa08944f43
Shannon Michaels <michaelsshan@seattleu.edu>
2019-08-11 18:08:00 -0700

    Change edge on iframes, tracked down display errors.

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
         <p>Do you know what the difference is between weather and climate? This video by the wonderful people at Crash Course explains the difference.</p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/YbAWny7FV3w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe  width="560" height="315" src="https://www.youtube.com/embed/YbAWny7FV3w" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
          <h2>The great Hank Green</h2>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ldLBoErAhz4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ldLBoErAhz4" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </template>
 
@@ -14,5 +15,6 @@
 </script>
 
 <style scoped>
+
 
 </style>
