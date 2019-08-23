3eaf0db653ccba76ba18aa455b2b87d8afbf4352
Shannon Michaels <michaelsshan@seattleu.edu>
2019-08-23 14:21:27 -0700

    Added links to the resources page

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -5,19 +5,20 @@
             NOAA
         </h3>
         <p>
-       link for noaa here.
+            <a href="https://www.weather.gov/documentation/services-web-api" target="_blank" alt="NOAA's weather api website">Website</a>
+
         </p>
         <h3>
            Open Weather Map
         </h3>
         <p>
-            link for OWM here.
+            <a href="https://openweathermap.org/api" target="_blank" alt="Open Weather Map's api website">Website</a>
         </p>
         <h3>
-          Crash Course
+          Crash Course/PBS Eons
         </h3>
         <p>
-            link for crash course here.
+            <a href="https://www.youtube.com/channel/UCzR-rom72PHN9Zg7RML9EbA" target="_blank" alt="Crash Course and PBS's Youtube channel for Eons">Website</a>
         </p>
     </div>
 </template>
