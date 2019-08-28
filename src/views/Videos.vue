a02f122d1ec06c865121ef43c821938051fca4ce
Shannon Michaels <michaelsshan@seattleu.edu>
2019-08-28 14:56:35 -0700

    Added content to Readme

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>Do you know what the difference is between weather and climate? This video by the wonderful people at Crash Course explains the difference.</p>
+        <h1>Videos</h1>
+        <p>Crash Course has a series of videos aimed at younger children that provide an introduction to climate science. In the video below, hosted by Sabrina Cruz, kids will find an answer to the question "Do you know what the difference is between weather and climate?"</p>
         <iframe  width="560" height="315" src="https://www.youtube.com/embed/YbAWny7FV3w" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
          <h2>The great Hank Green</h2>
