7dc04251dc8a6640e084d70cd0e983bca09081e7
Shannon Michaels <michaelsshan@seattleu.edu>
2019-08-31 14:36:02 -0700

    update main page graphics

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Videos</h1>
-        <p>Crash Course has a series of videos aimed at younger children that provide an introduction to climate science. In the video below, hosted by Sabrina Cruz, kids will find an answer to the question "Do you know what the difference is between weather and climate?"</p>
+        <p>One this page I hope to post my favorite educational videos on climate change. To start this off with a bang, Crash Course has a series of videos aimed at children, teens, and adults, that provide a brilliant introduction to climate science. Below are two videos, one aimed at younger children and hosted by Sabrina Cruz, and a advanced series call Eons, hosted by Crash Course creator Hank Green himself. These two videos are examples of the brilliant educational videos available on the web. This page is also an example of how flex-box can be used to create a responsive grid that re-aligns without media queries.</p>
         <ul class= "flex-container">
             <li class="flex-item">
              <iframe  width="560" height="315" src="https://www.youtube.com/embed/YbAWny7FV3w" frameborder="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -24,14 +24,15 @@
   .flex-container {
         display: flex;
         flex-wrap: wrap;
+        justify-content: space-around;
+        list-style-type: none;
 
   }
+    .flex-item li {
 
 
-.child {
-    width: 100px;
-    height: 100px;
-    margin: auto;
-}
+    }
+
+
 
 </style>
